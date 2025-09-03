779f1c2b2ab6097ddc21a21bf26d6d9fdb4c259e
hafsakk06-png <hafsakk06@gmail.com>
2025-09-03 03:58:00 -0700

    Add files via upload

---
+++ if-else.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout <<"enter number:";
+    cin >> n;
+
+    if(n%2 == 0) {
+        cout <<"even number\n";
+    } else {
+        cout <<"odd number\n";
+    }
+    return 0;
+}
