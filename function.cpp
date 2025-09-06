9da234d9e0af12662ecc70ab2d7c00bb84029ae1
Hafsa kk <hafsakk06@gmail.com>
2025-09-06 04:34:58 -0700

    do work hard

---
+++ function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+    // function definition
+    void printHello() {
+        cout <<"hello\n";
+    }
+
+    int main() {
+        // function call/invoke
+        printHello();
+        return 0;
+    }
